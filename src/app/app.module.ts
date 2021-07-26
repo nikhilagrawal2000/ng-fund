4fd6e9e1338fc26bae99ec610516c7c13e446513
nikhilagrawal2000 <nikhilagrawal1616@gmail.com>
2021-07-26 18:35:20 +0530

    Added toastr service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { ToastrService } from './common/toastr.service';
+import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/event.service';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
+  ],
+  providers: [EventService, ToastrService],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from '../common/toastr.service';
+import { EventService } from './shared/event.service';
+
+declare let toastr:any
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr/>
+        <div class="row">
+            <div *ngFor="let event of events" class="col-md-5">
+                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
+            </div>
+        </div>
+    </div>
+    `
+})
+
+export class EventsListComponent implements OnInit {
+    events: any[] = [];
+
+    constructor(private eventService: EventService, private toastr: ToastrService){
+
+    }
+
+    ngOnInit(){
+        this.events = this.eventService.getEvents()
+    }
+
+    handleThumbnailClick(eventName: any) {
+        this.toastr.success(eventName)
+    }
+
+}
